ae4890d383b9ec4c94221f306614226b7b170710
Oleg Kuzmin <manwoodt@student.21-school.com>
2023-09-01 14:47:18 +0300

    feat:add numbers to parser

--- src/main.c
+++ src/main.c
@@ -14,25 +14,31 @@ int main() {
   char *output_str = calloc(255, sizeof(char));
   // переделываем выражение из  классического в обратную польскую нотацию
   // scanf("%s", input_str);
-  strcpy(input_str, "5+6");
+  strcpy(input_str, "5465+6344");
   int lex_num_index = 0;
   int stek_index = 0;
   for (long unsigned int i = 0; i < strlen(input_str); i++) {
+    // операнды
     if (input_str[i] >= '0' && input_str[i] <= '9') {
       lex_num[lex_num_index] = input_str[i];
       lex_num_index++;
       // смотрим следующее символ - цифра, если да, добавляем в массив числа
       while (input_str[i + 1] >= '0' && input_str[i + 1] <= '9') {
         i++;
+        lex_num[lex_num_index] = input_str[i];
         lex_num_index++;
-        lex_num[i] = input_str[i];
       }
+      strcat(output_str, lex_num);
+      output_str[strlen(output_str)] = ' ';
+      memset(lex_num, '0', strlen(lex_num));
+      lex_num_index = 0;
+      // операции
     } else if (input_str[i] >= '(' && input_str[i] <= '/') {
       stek[stek_index] = input_str[i];
       stek_index++;
     }
   }
-  printf("%s", output_str);
+  printf("final output: %s\n", output_str);
   printf("final lex_num: %s\n", lex_num);
   printf("final stek: %s", stek);
 
