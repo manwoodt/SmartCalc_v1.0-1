2dc7c14b18621fdca631e24376bd694e2f18f698
Oleg Kuzmin <manwoodt@student.21-school.com>
2023-09-01 17:58:42 +0300

    fix: divide on some funcs

--- src/main.c
+++ src/main.c
@@ -1,42 +1,57 @@
 
 #include "calc.h"
-
+void parser(char *input_str, create_stack *output_str, create_stack *stack);
+void parser_operand(char *input_str, create_stack *output_str,
+                    long unsigned int *i);
 int main() {
+  // создаем стек и выходную строку
   create_stack output_str = {0};
   create_stack stack = {0};
   stack.char_or_not = 1;
   char *input_str = calloc(INPUT_STR_MAX_SIZE, sizeof(char));
-  char *lex_num = calloc(INPUT_STR_MAX_SIZE, sizeof(char));
+
   // scanf("%s", input_str);
   strcpy(input_str, "5465+6344-34534");
-  int lex_num_index = 0;
-  for (long unsigned int i = 0; i < strlen(input_str); i++) {
-    // операнды
-    if (input_str[i] >= '0' && input_str[i] <= '9') {
-      lex_num[lex_num_index] = input_str[i];
-      lex_num_index++;
-      // смотрим следующее символ - цифра, если да, добавляем в массив числа
-      while (input_str[i + 1] >= '0' && input_str[i + 1] <= '9') {
-        i++;
-        lex_num[lex_num_index] = input_str[i];
-        lex_num_index++;
-      }
-      output_str.data[output_str.size] = atoi(lex_num);
-      output_str.size++;
-      memset(lex_num, '0', strlen(lex_num));
-      lex_num_index = 0;
-      // операции
-    } else if (input_str[i] >= '(' && input_str[i] <= '/') {
-      push(&stack, input_str[i]);
-    }
-  }
+
+  parser(input_str, &output_str, &stack);
+
   printf("Stack\n");
   printStack(stack);
   printf("Output_str\n");
   printStack(output_str);
 
   free(input_str);
-  free(lex_num);
 
   return 0;
+}
+
+void parser(char *input_str, create_stack *output_str, create_stack *stack) {
+  for (long unsigned int i = 0; i < strlen(input_str); i++) {
+    // операнды
+    if (input_str[i] >= '0' && input_str[i] <= '9') {
+      parser_operand(input_str, output_str, &i);
+
+      // операции
+    } else if (input_str[i] >= '(' && input_str[i] <= '/') {
+      push(stack, input_str[i]);
+    }
+  }
+}
+
+void parser_operand(char *input_str, create_stack *output_str,
+                    long unsigned int *i) {
+  char *lex_num = calloc(INPUT_STR_MAX_SIZE, sizeof(char));
+  int lex_num_index = 0;
+  lex_num[lex_num_index] = input_str[*i];
+  lex_num_index++;
+  // смотрим следующее символ - цифра, если да, добавляем в массив числа
+  while (input_str[*i + 1] >= '0' && input_str[*i + 1] <= '9') {
+    (*i)++;
+    lex_num[lex_num_index] = input_str[*i];
+    lex_num_index++;
+  }
+  output_str->data[output_str->size] = atoi(lex_num);
+  output_str->size++;
+  memset(lex_num, '0', strlen(lex_num));
+  free(lex_num);
 }
