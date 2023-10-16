af2a50db6d7cd6a4cb8ef76864b5628a030964e4
Oleg Kuzmin <manwoodt@student.21-school.com>
2023-10-16 16:12:04 +0300

    order in .h

--- src/calc.h
+++ src/calc.h
@@ -1,14 +1,11 @@
 #ifndef S21_STRING_H
 #define S21_STRING_H
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// #include <ctype.h>
-#include <math.h>
-// #include <stdarg.h>
-// #include <stdint.h>
 #define STACK_OVERFLOW 1
 #define STACK_UNDERFLOW 2
 #define STACK_MAX_SIZE 255
@@ -23,19 +20,20 @@ typedef struct {
   int priority[STACK_MAX_SIZE];
 } create_stack;
 
-void printStack(create_stack stack);
 void push(create_stack *stack, double value);
 double pop(create_stack *stack);
 double peek(const create_stack *stack);
 void nulldata(create_stack *stack);
 int priority(int operation);
+int is_good_expression(char *expression);
+
 int validator(const char *input_str, char *cor_input_str, int is_there_x_value,
               int is_x_correct);
 int garbage_for_validator(int operation);
-
-int is_good_expression(char *expression);
-
-void number_w_dot(const char *input_str, char *num_in_arr, unsigned int i);
+int trigonometry_change(const char *input_str, char *cor_input_str,
+                        unsigned int *i, unsigned int *j);
+void unary(char *input_str, char znak, int *array, unsigned int i,
+           int *correct);
 int how_much_dots(char *input_str, unsigned int i);
 void replacement_x(char *cor_input_str, char *cor_input_str_with_x,
                    char *ch_x_value);
@@ -44,16 +42,12 @@ int is_operation(int operation);
 int is_number(int operation);
 int is_trigonometry(int operation);
 
-int trigonometry_change(const char *input_str, char *cor_input_str,
-                        unsigned int *i, unsigned int *j);
-void unary(char *input_str, char znak, int *array, unsigned int i,
-           int *correct);
-
 double parser(char *input_str, char *x_value);
 void parser_operand(const char *input_str, create_stack *output_str,
                     unsigned int *i);
 void parser_operation(char *input_str, create_stack *stack,
                       create_stack *output_str, unsigned int i);
+void number_w_dot(const char *input_str, char *num_in_arr, unsigned int i);
 
 double calculation(create_stack *output_str);
 void calculation_operation(create_stack *output_str, create_stack *stack_res,
@@ -64,6 +58,7 @@ void calculation_basic_operation(create_stack *output_str,
 void calculation_math_func_operation(create_stack *output_str,
                                      create_stack *stack_res, unsigned int i,
                                      double num1);
+
 // credit
 int calculateAnnuity(double loanAmount, int term, double interestRate,
                      double *monthlyPayment, double *totalPayment,
